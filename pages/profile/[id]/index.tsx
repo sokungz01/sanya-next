18fc2182e6e170f98efaf0fa261c627c7619572f
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-10-05 15:59:43 +0700

    Fix duplicate route

--- pages/profile/[id]/index.tsx
+++ pages/profile/[id]/index.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useContext, useState } from "react";
+import TabBar from "../../../components/tabbar";
+import PersonalInformation from "../../../components/profile/personal-information/index";
 
-type Props = {}
+const Profile = () => {
+  const router = useRouter();
+  //   const { user } = useContext;
+  //   const { data: session, status } = useSession({ required: true });
+  const { id } = router.query;
+  const [active, setActive] = useState("ข้อมูลส่วนตัว");
 
-const Profile = (props: Props) => {
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <TabBar
+        active={active}
+        setActive={setActive}
+        tabs={[
+          "ข้อมูลส่วนตัว",
+          "สัญญาสำเร็จรูป",
+          "ข้อความและออเดอร์",
+          "รายการที่ชื่นชอบ",
+        ]}
+      />
+      {active == "ข้อมูลส่วนตัว" ? (
+        <PersonalInformation />
+      ) : active == "สัญญาสำเร็จรูป" ? (
+        <div>สัญญาสำเร็จรูป</div>
+      ) : active == "ข้อความและออเดอร์" ? (
+        <div>ข้อความและออเดอร์</div>
+      ) : active == "รายการที่ชื่นชอบ" ? (
+        <div>รายการที่ชื่นชอบ</div>
+      ) : null}
+    </>
+  );
+};
+
+export default Profile;
