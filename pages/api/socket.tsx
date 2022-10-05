aa62e362060abeaa009f0528ee41ef0b689c7bf0
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-10-05 10:47:43 +0700

    +socket

---
+++ pages/api/socket.tsx
@@ -0,0 +1,19 @@
+import { Server } from "socket.io";
+import messageHandler from "../../utils/socket/messageHandler";
+
+export default function SockerHandler(req, res) {
+  if (res.socket.server.io) {
+    console.log("Already set up");
+    res.send();
+    return;
+  }
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
+  const onConnection = (socket) => {
+    messageHandler(io, socket);
+  };
+
+  io.on("connection", onConnection);
+  console.log("Setting up socket");
+  res.end();
+}
